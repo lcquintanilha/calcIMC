1f79426f979459a39ab555e9662607aefcd1d328
Lucas Quintanilha <lcquintanilha6@hotmail.com>
2023-09-11 08:20:56 -0300

    some minor changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,54 +1,12 @@
-function meuEscopo(){
+const elementos = [
+    {tag: 'p', texto: 'Frase 1'},
+    {tag: 'div', texto: 'Frase 2'},
+    {tag: 'footer', texto: 'Frase 3'},
+    {tag: 'section', texto: 'Frase 4'},
+    ];
 
-    const peso = document.querySelector("#peso");
-    const altura = document.querySelector("#altura");
-    const form = document.querySelector(".form");
 
-
-    function enviar(evento){
-        evento.preventDefault();
-        
-        const spanAltura = document.querySelector("#spanAltura");
-        const spanPeso = document.querySelector("#spanPeso");
-        const resultado = document.querySelector("#resultado");
-
-        if(altura.value === ""){
-            spanAltura.style.display = "inline-block";
-            altura.style.border = "solid 1px red";
-        }else{
-            spanAltura.style.display = "none";
-            altura.style.border = "solid black 1px";
-        }
-        
-        if(peso.value === ""){
-            spanPeso.style.display = "inline-block";
-            peso.style.border = "solid 1px red";
-        }else{
-            spanPeso.style.display = "none";
-            peso.style.border = "solid 1px black";
-        }
-
-        if(altura.value !== "" && peso.value !== ""){
-            let imc = peso.value/((altura.value/100)**2);
-            resultado.style.display = "block"
-
-            if(imc < 18.5){
-                resultado.innerHTML = `O seu IMC é de ${imc.toFixed(2)}.<br>Você está <strong>abaixo do peso</strong>`
-            }else if(imc < 25){
-                resultado.innerHTML = `O seu IMC é de ${imc.toFixed(2)}.<br>Você está com o <strong>peso normal</strong>`
-            }else if(imc < 30){
-                resultado.innerHTML = `O seu IMC é de ${imc.toFixed(2)}.<br>Você está com <strong>sobrepeso</strong>`
-            }else if(imc < 35){
-                resultado.innerHTML = `O seu IMC é de ${imc.toFixed(2)}.<br>Você está com <strong>Obesidade grau 1</strong>`
-            }else if(imc <= 40){
-                resultado.innerHTML = `O seu IMC é de ${imc.toFixed(2)}.<br>Você está com <strong>Obesidade grau 2</strong>`
-            }else if(imc > 40){
-                resultado.innerHTML = `O seu IMC é de ${imc.toFixed(2)}.<br>Você está com <strong>Obesidade grau 3</strong>`
-            }
-        }
+    const array = ["um", "dois", "tres"];
+    for (let i in array){
+        console.log(array[i]);
     }
-
-    form.addEventListener('submit', enviar);
-}
-
-meuEscopo()
